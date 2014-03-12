ccd6daf401a94363268575e81a252ce871b3069c
Cameron Massoudi <cameronmassoudi@moobilenet-106-215.ucdavis.edu>
2014-03-11 23:48:35 -0700

    main opens and parses file

--- Main.java
+++ Main.java
@@ -1,18 +1,50 @@
 import java.nio.file.*;
 import java.io.*;
+import java.util.*;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 public class Main
 {
 	public static void main(String[] args)
-	{	File file = new File(args[0]);
-		InputStream in = Files.newInputStream(file);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		String line = null;
-
-		while((line = reader.readLine()) != null)
+	{	
+		if(args.length != 1) 
 		{
-			System.out.println(line);
+  			System.err.println("Invalid command line, exactly one argument required");
+  			System.exit(1);
 		}
 
+		try
+		{
+            // Open the file that is the first 
+            // command line parameter
+            FileInputStream fstream = new FileInputStream(args[0]);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null)   {
+              // Print the content on the console
+              System.out.println (strLine);
+            }
+            //Close the input stream
+            //in.close();
+            }catch (Exception e){//Catch exception if any
+              System.err.println("Error: " + e.getMessage());
+            }
+
 	}
+
 }
