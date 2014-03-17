7e61be767c44a2cccc2aaf39b0e711d2e5a1e825
Cameron Massoudi <cameronmassoudi@Cameron-Massoudis-MacBook-Pro.local>
2014-03-16 17:21:20 -0700

    Data class now takes file name for constructer parameter

--- Data.java
+++ Data.java
@@ -1,7 +1,9 @@
+import java.io.*;
+import java.util.*;
 
 public class Data
 {
-	public int image2DArray[][];
+	public int[][] image2DArray = new int[120][128];
 	public double sex;
 
 	public static double FEMALE = 0.1;
@@ -12,4 +14,64 @@ public class Data
 		sex = s;
 		image2DArray = array;
 	}
+
+	public Data(double s, String file)
+	{
+    sex = s;
+		try
+    {
+      FileInputStream fstream = new FileInputStream(file);
+            System.out.println("file: " + file);
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+      String strLine;
+      //Read File Line By Line
+      int row = 0;
+      int col = 0;
+      int chunk = 0;
+      while ((strLine = br.readLine()) != null)
+      {
+        // System.out.println("strLine: " + strLine);
+
+        String[] strArray = strLine.split(" ");
+        int[] intArray = new int[strArray.length];
+        
+        for(int j = 0; j < strArray.length; j++) 
+        {
+            intArray[j] = Integer.parseInt(strArray[j]);
+        }
+
+        if(chunk >= 8)
+        {
+          chunk = 0;
+          row++;
+        }
+
+        if(chunk >= 0)
+        {
+          chunk++;
+        }
+
+        for(int k = 0; k < 16; k++)
+        {
+          image2DArray[row][col] = intArray[k];
+          col++;
+        }
+
+        if(col == 128)
+        {
+          col = 0;
+        }
+       
+       }
+      
+      }
+  
+
+      catch (Exception e)
+        {
+          //Catch exception if any
+          System.err.println("Error: " + e.getMessage());
+        }
+  }
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.nio.file.*;
+//import java.nio.file.*;
 import java.io.*;
 import java.util.*;
 
@@ -28,54 +28,11 @@ public class Main
       { 
           try
     		  {
-                // Open the file that is the first 
-                // command line parameter
-                FileInputStream fstream = new FileInputStream(args[i]);
-                BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-                String strLine;
-                //Read File Line By Line
-                int[][] image = new int[120][128];
-                int row = 0;
-                int col = 0;
-                int chunk = 0;
-                while ((strLine = br.readLine()) != null)
-                {
-                  String[] strArray = strLine.split(" ");
-                  int[] intArray = new int[strArray.length];
-                  
-                  for(int j = 0; j < strArray.length; j++) 
-                  {
-                      intArray[j] = Integer.parseInt(strArray[j]);
-                  }
-
-                  if(chunk >= 8)
-                  {
-                    chunk = 0;
-                    row++;
-                  }
-
-                  else if(chunk > 0)
-                  {
-                    chunk++;
-                  }
-
-                  for(int k = 0; k < 16; k++)
-                  {
-                    image[row][col] = intArray[k];
-                    col++;
-                  }
-
-                  if(col == 128)
-                  {
-                    col = 0;
-                  }
-
-                }
-                //Close the input stream
-                //in.close();
-                Data d = new Data(0.1, image);
-                for(int rows = 0; row < 120; row++)
-                { System.out.println();
+                Data d = new Data(0.1, args[i]);
+                System.out.println("created Data object");
+                for(int rows = 0; rows < 120; rows++)
+                { 
+                  System.out.println();
                   for(int cols = 0; cols < 128; cols++)
                   {
                     System.out.print(d.image2DArray[rows][cols]);
@@ -87,12 +44,12 @@ public class Main
 
           }
 
-
-        catch (Exception e)
+          catch (Exception e)
         {
           //Catch exception if any
           System.err.println("Error: " + e.getMessage());
         }
+
       }
       i++;
     }
